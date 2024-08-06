c2126541276cc26c9adc01e6a40ebc829fd7fa1e
Muhammed Yaseen Sidhik <mhdyaseensid@gmail.com>
2024-08-06 18:37:31 +0530

    registration form completed

--- public/scripts/registrationScripts.js
+++ public/scripts/registrationScripts.js
@@ -2,35 +2,44 @@
 
 import { months } from "/scripts/utils.js";
 
-const dateInput = document.getElementById("dob");
-const currentDateText = document.getElementById("currentDate");
-const age = document.getElementById("age");
+document.addEventListener("DOMContentLoaded", () => {
+  const dateInput = document.getElementById("dob");
+  const currentDateText = document.getElementById("currentDate");
+  const age = document.getElementById("age");
+  const currentAge = document.getElementById("currentAge");
 
-const todaysDate = new Date();
-const currentMonth = months[todaysDate.getMonth()];
-const currentDate = todaysDate.getDate();
-const currentYear = todaysDate.getFullYear();
+  const todaysDate = new Date();
+  const currentMonth = months[todaysDate.getMonth()];
+  const currentDate = todaysDate.getDate();
+  const currentYear = todaysDate.getFullYear();
+  const maxYear = currentYear - 17;
+  const maxMonth = (todaysDate.getMonth() + 1).toString().padStart(2, "0");
+  const maxDay = currentDate.toString().padStart(2, "0");
+  const maxDate = `${maxYear}-${maxMonth}-${maxDay}`;
 
-currentDateText.textContent = `${currentMonth} ${currentDate}, ${currentYear}`;
+  currentDateText.textContent = `${currentMonth} ${currentDate}, ${currentYear}`;
 
-dateInput.addEventListener("change", (e) => {
-  const selectedDate = new Date(dateInput.value);
-  let years = currentYear - selectedDate.getFullYear();
-  let months = todaysDate.getMonth() - selectedDate.getMonth();
-  let days = currentDate - selectedDate.getDate();
+  dateInput.addEventListener("change", (e) => {
+    const selectedDate = new Date(dateInput.value);
+    let years = currentYear - selectedDate.getFullYear();
+    let months = todaysDate.getMonth() - selectedDate.getMonth();
+    let days = currentDate - selectedDate.getDate();
 
-  if (days < 0) {
-    months--;
-    days += new Date(
-      todaysDate.getFullYear(),
-      todaysDate.getMonth(),
-      0
-    ).getDate();
-  }
-  if (months < 0) {
-    years--;
-    months += 12;
-  }
+    if (days < 0) {
+      months--;
+      days += new Date(
+        todaysDate.getFullYear(),
+        todaysDate.getMonth(),
+        0
+      ).getDate();
+    }
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+    currentAge.value = years;
+    age.value = `${years} years, ${months} months, ${days} days old`;
+  });
 
-  age.value = `${years} years, ${months} months, ${days} days old`;
+  dateInput.max = maxDate;
 });
